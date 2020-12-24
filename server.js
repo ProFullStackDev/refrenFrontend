02aaebcec13ca50ddd08f7ecca55932030aac376
GodFather93 <suryapalsingh.inda@gmail.com>
2020-12-24 19:43:50 +0530

    yarn removed

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const path = require('path');
+
+const port = process.env.PORT || 8080;
+
+const app = express();
+
+const publicPath = path.join(__dirname, '.', 'dist');
+console.log(path.join(__dirname, 'dist', 'index.html'));
+app.use(express.static(publicPath));
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}!`);
+});
